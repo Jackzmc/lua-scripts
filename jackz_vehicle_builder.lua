ba7d6842a71f4935de231a3fc161ce2f805f54ca
Jackz <jacksonbixby@gmail.com>
2022-08-28 22:58:02 -0500

    Disable collision between parent/child

--- jackz_vehicle_builder.lua
+++ jackz_vehicle_builder.lua
@@ -1873,6 +1873,7 @@ function _load_attach_list(list, child)
         if handle ~= child and handle ~= builder.base.handle and builder.entities[handle].parent ~= builder.entities[child].id and builder.entities[child].parent ~= data.id then
             table.insert(attachEntSubmenus, menu.action(list, data.name or ("Unnamed " .. data.type), {}, string.format("Handle: %s\nType: %s", handle, data.type), function()
                 builder.entities[child].parent = builder.entities[handle].id
+                ENTITY.SET_ENTITY_NO_COLLISION_ENTITY(handle, child)
                 attach_entity(handle, child, builder.entities[child].pos, builder.entities[child].rot, builder.entities[child].boneIndex)
                 util.toast("Entity's parent changed")
                 menu.set_menu_name(list, "Attach to: #" .. builder.entities[child].id)
@@ -2322,6 +2323,7 @@ function add_attachments(baseHandle, data, addToBuilder, isPreview)
         if not targetHandle then
             log("Invalid parent handle: " .. entry.data.parent .. " for id " .. entry.id, "add_attachments")
         else
+            ENTITY.SET_ENTITY_NO_COLLISION_ENTITY(entry.handle, targetHandle)
             attach_entity(targetHandle, entry.handle, entry.data.offset, entry.data.rotation, entry.data.boneIndex)
         end
     end
