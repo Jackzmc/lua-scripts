8eacdec172609f79f358f3191d7e73c52d2b7d98
Jackz <me@jackz.me>
2022-09-07 13:35:38 -0500

    Bump versions

--- jackz_vehicle_builder.lua
+++ jackz_vehicle_builder.lua
@@ -1,7 +1,7 @@
 -- Jackz Vehicle Builder
 -- SOURCE CODE: https://github.com/Jackzmc/lua-scripts
 local SCRIPT = "jackz_vehicle_builder"
-VERSION = "1.21.3"
+VERSION = "1.21.4"
 local LANG_TARGET_VERSION = "1.3.3" -- Target version of translations.lua lib
 local VEHICLELIB_TARGET_VERSION = "1.3.1"
 
