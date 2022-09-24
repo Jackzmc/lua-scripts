5af48ae06f23ef73cc37d050b96bcb8f56984271
Jackz <jacksonbixby@gmail.com>
2022-09-24 11:31:43 -0500

    Changelog

--- jackz_vehicle_builder.lua
+++ jackz_vehicle_builder.lua
@@ -1,7 +1,7 @@
 -- Jackz Vehicle Builder
 -- SOURCE CODE: https://github.com/Jackzmc/lua-scripts
 local SCRIPT = "jackz_vehicle_builder"
-VERSION = "1.24.8"
+VERSION = "1.24.9"
 local LANG_TARGET_VERSION = "1.3.3" -- Target version of translations.lua lib
 local VEHICLELIB_TARGET_VERSION = "1.3.1"
 local ANIMATOR_LIB_TARGET = "1.0.0"
