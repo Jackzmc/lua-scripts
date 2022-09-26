6df6e1cfdac2a775c299f057ed0986274b35f06f
Jackz <jacksonbixby@gmail.com>
2022-09-26 12:06:20 -0500

    Refactor timer and json systems

--- lib/jackzscript/core/util.lua
+++ lib/jackzscript/core/util.lua
@@ -27,35 +27,59 @@ function jutil.WriteKV(file, kv, prefix)
     file:flush()
 end
 
--- TODO: Allow stopping timer
---- Creates a timer that is called every millisecond
+local _timers = {}
+local _timer_id = 1
+function _get_timer_id()
+    local id = _timer_id
+    _timer_id = _timer_id + 1
+    _timers[id] = true
+    return id
+end
+
+--- Creates a timer that is called every millisecond. Use jtuil.StopTimer to cancel
 --- @param callback function A function to be called every interval
 --- @param ... any arguments to pass to callback
+--- @returns number Returns a timer id
 function jutil.CreateTimer(ms, callback, ...)
     if not ms or not callback then error("Missing one or more properties: 'ms', and 'callback'") end
     local data = {...}
+    local timerId = _get_timer_id()
     util.create_tick_handler(function()
         callback(data)
         util.yield(ms)
-        return true
+        return _timers[timerId]
     end)
+    return timerId
 end
 
--- TODO: Allow stopping timeout
---- Creates a timeout that is called after the specified amount of ms elapses
+--- Creates a timeout that is called after the specified amount of ms elapses. Use jtuil.StopTimer to cancel
 --- @param callback function A function to be called once time is up
 --- @param ... any arguments to pass to callback
+--- @returns number Returns a timer id
 function jutil.CreateTimeout(ms, callback, ...)
     if not ms or not callback then error("Missing one or more properties: 'ms', and 'callback'") end
     local data = {...}
+    local timerId = _get_timer_id()
     util.create_tick_handler(function()
         ms = ms - 1
         if ms <= 0 then
             callback(data)
             return false
         end
-        return true
+        return _timers[timerId]
     end)
+    return timerId
+end
+
+--- Creates a timeout that is called after the specified amount of ms elapses. Use jtuil.StopTimer to cancel
+--- @param callback function A function to be called once time is up
+--- @param ... any arguments to pass to callback
+--- @returns boolean returns true if timer was valid
+function jutil.StopTimer(timerId)
+    local result = false
+    if _timers[timerId] then result = true end
+    _timers[timerId] = nil
+    return result
 end
 
 function jutil.ParseSemver(version)
@@ -95,52 +119,71 @@ function jutil.DumpTable(o)
     end
 end
 
--- TODO: Replace sync return with async callback
---- Fetches a URL and parses JSON.
---- @param type string either 'GET' or 'POST'
---- @param uri string The full url to fetch from
---- @param headers Record<string, string> Key-value list of headers
---- @param payload ?string A payload optionally to send
---- @return any json object or false on invalid json, or nil on 204
-function jutil.FetchJson(type, uri, headers, payload)
+--- Fetches json via GET
+--- @param uri string The full url (with domain) to fetch from
+--- @param headers ?Record<string, string> Key-value list of headers, optional
+--- @param payload ?string A payload optionally to send, will be sent as serialized json (don't encode before)
+--- @param successCallback function Called with json results (statusCode, resultHeaders, result). If status code is 204, result is nil
+--- @param errorCallback ?function Optionally called with any errors. (statusCode, resultHeaders, errorMessage) Network error is -1 
+function jutil.GetJson(uri, headers, successCallback, errorCallback)
+    _doJsonRequest(uri, headers, nil, successCallback, errorCallback)
+end
+
+--- POST to a URL and parses JSON.
+--- @param uri string The full url (with domain) to fetch from
+--- @param headers ?Record<string, string> Key-value list of headers, optional
+--- @param payload ?string A payload optionally to send, will be sent as serialized json (don't encode before)
+--- @param successCallback function Called with json results (statusCode, resultHeaders, result). If status code is 204, result is nil
+--- @param errorCallback ?function Optionally called with any errors. (statusCode, resultHeaders, errorMessage) Network error is -1 
+function jutil.PostJson(uri, headers, payload, successCallback, errorCallback)
+    _doJsonRequest(uri, headers, payload, successCallback, errorCallback)
+end
+
+function _doJsonRequest(type, uri, headers, payload, successCallback, errorCallback)
+    if not successCallback then
+        error("Missing callback for successful requests")
+    end
+    
     local domain, path = uri:match("([a-zA-Z.-]+)(/.*)")
     if not domain then
         error("Invalid URI provided: " .. uri)
     end
-    local result = nil
-    async_http.init(domain, path, function(body, res_headers, status_code)
-        if status_code == 200 then
-            local status, obj = pcall(json.decode, body)
-            if status then
-                jutil.DumpTable(obj)
-                result = obj
-            else
-                result = false
+    async_http.init(
+        domain, path, 
+        function(body, resHeaders, statusCode)
+            if statusCode == 200 then
+                local status, obj = pcall(json.decode, body)
+                if status then
+                    successCallback(statusCode, resHeaders, obj)
+                elseif errorCallback ~= nil then
+                    errorCallback(statusCode, resHeaders, "Invalid json result: " .. obj)
+                end
+            elseif statusCode == 204 then
+                successCallback(statusCode, resHeaders, nil)
+            elseif errorCallback ~= nil then
+                errorCallback(statusCode, resHeaders, "Server returned status code " .. statusCode)
             end
-        elseif status_code == 204 then
-            return nil
-        else
-            error("Server returned status code " .. status_code)
+        end,
+        function()
+            if errorCallback ~= nil then
+                errorCallback(-1, {}, "Network Error")
+            end
         end
-    end, function()
-        error("Network Error")
-    end)
+    )
+
     if type == "POST" then
-        async_http.set_post("application/json", payload or "")
+        async_http.set_post("application/json", payload and json.encode(payload) or "")
     elseif type ~= "GET" then
         error("Invalid method type, only GET or POST is supported.")
     end
+
     if headers then
         for k, v in pairs(headers) do
             async_http.add_header(k, v)
         end
     end
+
     async_http.dispatch()
-    -- Wait for download to complete
-    while result == nil do
-        util.yield()
-    end
-    return result
 end
 
 function jutil.ShowBusySpinner(text)
