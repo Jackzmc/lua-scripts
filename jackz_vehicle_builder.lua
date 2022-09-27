a307aff6c70354336da03594de4dbeeb0dca137d
Jackz <jacksonbixby@gmail.com>
2022-09-26 20:34:42 -0500

    Support new recording format

--- jackz_vehicle_builder.lua
+++ jackz_vehicle_builder.lua
@@ -1,7 +1,7 @@
 -- Jackz Vehicle Builder
 -- SOURCE CODE: https://github.com/Jackzmc/lua-scripts
 local SCRIPT = "jackz_vehicle_builder"
-VERSION = "1.24.12"
+VERSION = "1.25.0"
 local LANG_TARGET_VERSION = "1.3.3" -- Target version of translations.lua lib
 local VEHICLELIB_TARGET_VERSION = "1.3.1"
 local ANIMATOR_LIB_TARGET = "1.0.0"
@@ -2502,12 +2502,13 @@ function setup_animations_list(list, entity)
         return
     end
     animatorLib.RecordingController.ListRecordings(function(filepath, filename)
-        table.insert(animationsList, menu.action(list, filename, {}, "Click to use this animation for this entity.\n\nFilepath: " .. filepath, function()
+        table.insert(animationsList, menu.action(list, filename, {}, "Click to use this animation for this entity. Only compatible with newer version formats (ones that start with Recording YY-MM-DD)\n\nFilepath: " .. filepath, function()
             local data = animatorLib.RecordingController.LoadRecordingData(filepath)
             builder.entities[entity].customAnimation = data
             PlaybackController:StartPlayback(entity, data.points, data.interval, {
                 ["repeat"] = true,
-                debug = true
+                debug = true,
+                positionsFormat = 2
             })
         end))
     end)
